af031cd39f6693dd152acbf75a042b46bcb4ba23
Niño Allen Ardiente <nardiente@nerubia.com>
2025-02-25 15:54:55 +0800

    fix: Add page reload on onboarding success route

--- src/pages/onboarding/onboarding.tsx
+++ src/pages/onboarding/onboarding.tsx
@@ -34,6 +34,7 @@ const OnboardingPage = () => {
     if (window.location.pathname === '/ob-success') {
       localStorage.setItem('onboarding_page', OnboardingPages.SUCCESS);
       setActivePage(OnboardingPages.SUCCESS);
+      window.location.reload();
       return;
     }
     setActivePage(localStorage.getItem('onboarding_page') as OnboardingPages);
